4b8703b2d532a1a674d2a0db987fe134bc7fc8eb
stfeleti <89973353+stfeleti@users.noreply.github.com>
2022-06-23 16:58:29 +0200

    gulp2 changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,32 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+// process JS files and return the stream.
+gulp.task('js', function() {
+    return gulp.src('js/*js')
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
 
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['js'], function(done) {
+    browserSync.reload();
+    done();
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('default', ['js'], function() {
+
+    // Serve files from the root of this project
     browserSync.init({
-        server: "./app"
+        server: {
+            baseDir: "./"
+        }
     });
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("js/*.js", ['js-watch']);
 });
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('default', ['serve']);
