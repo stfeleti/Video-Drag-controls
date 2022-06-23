0681ceb79328fdd2f8c4b0102c44f429140ff3c4
stfeleti <89973353+stfeleti@users.noreply.github.com>
2022-06-23 16:57:28 +0200

    gulp changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,24 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
 
-// Basic Gulp task syntax
-gulp.task('hello', function() {
-    console.log('Hello Zell!');
-})
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-// Development Tasks 
-// -----------------
-
-// Start browserSync server
-gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        }
-    })
-})
-
-
-// Watchers
-gulp.task('watch', function() {
-    gulp.watch('**/*.css', browserSync.reload);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-})
-
-// Optimization Tasks 
-// ------------------
-
-// Optimizing CSS and JavaScript 
-gulp.task('useref', function() {
-
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('dist'));
-});
-
-// Optimizing Images 
-gulp.task('images', function() {
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            interlaced: true,
-        })))
-        .pipe(gulp.dest('dist/images'))
-});
-
-// Copying fonts 
-gulp.task('fonts', function() {
-    return gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
-})
-
-// Cleaning 
-gulp.task('clean', function() {
-    return del.sync('dist').then(function(cb) {
-        return cache.clearAll(cb);
+    browserSync.init({
+        server: "./app"
     });
-})
 
-gulp.task('clean:dist', function() {
-    return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-// Build Sequences
-// ---------------
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
 
-gulp.task('default', function(callback) {
-    runSequence(['browserSync'], 'watch',
-        callback
-    )
-})
-
-gulp.task('build', function(callback) {
-    runSequence(
-        'clean:dist', ['useref', 'images', 'fonts'],
-        callback
-    )
-})
+gulp.task('default', ['serve']);
